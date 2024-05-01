fbc7302aeb55f781ebbec7ec86dc04d8b00bf713
Nabeel Khan <nabeel.khan@bbc.co.uk>
2024-05-02 00:15:10 +0100

    add public files folder where files will be saved to avoid cluttering the main directory

---
+++ public_files/README.md
@@ -0,0 +1,4 @@
+# Computer Architecture and Network Systems (COMPSCI2027) 2023-24
+
+Repository for code used in lectures, labs and assessed exercises.
+
---
+++ public_files/examples6.js
@@ -0,0 +1,21 @@
+// Importing the 'http' module
+const http = require('http');
+
+// Define the hostname and port on which the server will listen
+const hostname = '127.0.0.1'; // Localhost
+const port = 3000; // Port 3000
+
+// Creating the server
+const server = http.createServer((req, res) => {
+  // Setting the status code and content type of the response
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  
+  // Sending a response back to the client
+  res.end('THIS IS THE SIXTH VERSION, World!\n');
+});
+
+// Start the server and listen on the specified port and hostname
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
